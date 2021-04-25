6f3fc8f9e05831d35de9258dd81f73f7206f8f06
98Raymond25 <79771930+98Raymond25@users.noreply.github.com>
2021-04-24 21:27:17 -0400

    Create Conexion.cs

---
+++ Project/Services/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperAlquiler.Services
+{
+    public class Conexion
+    {
+        SqlConnection con = null;
+
+        private void Conectar()
+        {
+            if (con == null)
+            {
+                con = new SqlConnection("Server=tcp:gelmo-server.database.windows.net,1433;Initial Catalog=GelmoDatabase;Persist Security Info=False;User ID=gelmoadmin;Password=GelmoPassword71;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+    }
+}
